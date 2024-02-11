5cccf06aae1962823e6a15bb4d7f7ed8d5789030
desboisGIT <robchail@free.fr>
2024-02-11 17:05:48 +0100

    path finding in work

--- crafty.py
+++ crafty.py
@@ -22,14 +22,8 @@ gravity = 0.1
 friction =0.7
 jmpForce = 3
 speed = 15
-###---------------- TEXTURE ----------------###
 
-dirt = pygame.image.load("textures/dirt.png")
-grass = pygame.image.load("textures/grass.png")
-stone = pygame.image.load("textures/stone.png")
-
-###---------------- TEXTURE ----------------###
-Xvel,Yvel = 0,0
+Xvel,Yvel,grav = 0,0,0
 
 
 class Player:
@@ -88,6 +82,84 @@ class map():
                 if tiles[i-1][j][2][0] != 'air' and tiles[i][j][2][0] == "grass":
                     tiles[i][j][2] = ['dirt',(80,40,0)]
                 pygame.draw.rect(screen, tiles[i][j][2][1], pygame.Rect(tiles[i][j][0], tiles[i][j][1], tileSize, tileSize))
+
+class Mobs:
+    def __init__(self, x, y):
+        self.posx = x
+        self.posy = y
+        self.width = 16
+        self.height = 32
+        self.on_ground = False  # Reset on_ground flag
+          # Flag to indicate if the player is on the ground
+
+    def move(self, dx, dy):
+
+        self.posx += dx
+        self.posy += dy
+        
+        # Check for collisions with non-air blocks
+
+        for i in range(len(tiles)):
+            for j in range(len(tiles[0])):
+                if tiles[i][j][2][0] != 'air':
+                    block_rect = pygame.Rect(tiles[i][j][0], tiles[i][j][1], tileSize, tileSize)
+                    mob_rect = pygame.Rect(self.posx, self.posy, self.width, self.height)
+                    if block_rect.colliderect(mob_rect):
+                        # If collision detected, adjust player position to avoid collision
+                        if dx > 0:
+                            self.posx = tiles[i][j][0] - self.width
+                        elif dx < 0:
+                            self.posx = tiles[i][j][0] + tileSize
+                        if dy > 0:
+                            self.posy = tiles[i][j][1] - self.height
+                            self.on_ground = True
+                            
+                        elif dy < 0:
+                            self.posy = tiles[i][j][1] + tileSize
+    def mobPathFind(self):
+        posMob = [self.posx, self.posy]
+        posPlayer = [player1.posx, player1.posy]
+        if posMob[0] < posPlayer[0]:
+            print("TO THE RIGHTTT")
+        elif posMob[0] > posPlayer[0]:
+            print("TO THE LEFFTTT")
+
+        iY,iX,indexY,indexX=0,0,0,0
+        while iY < posMob[1]:
+            iY+=tileSize
+            indexY+=1
+            while iX < posMob[0]:
+                iX+=tileSize
+                indexX+=1
+        indexX -=1
+        indexY -=1 
+        try:
+
+            if tiles[indexY][indexX][2][0] == 'air':                    #spagueti gusto
+                if (tiles[indexY+1][indexX][2][0] != 'air' or 
+                    tiles[indexY][indexX+1][2][0] != 'air' or 
+                    tiles[indexY-1][indexX][2][0] != 'air' or 
+                    tiles[indexY][indexX-1][2][0] != 'air'):
+                        tiles[indexY][indexX][2] = sellected_block
+        except:
+            print("WHAT THE FUK BRO TF IS THAT ?!")
+
+
+
+
+
+"""
+        if posMob[1] < posPlayer[1]:
+            print("TO THE BOTTOM")
+        elif posMob[1] > posPlayer[1]:
+            print("TO THE TOPPP")
+"""
+
+
+
+
+
+mob1 = Mobs(200, 100)
 player1 = Player(100, 100)
 map.generateMap()
 map.updateMap()
@@ -153,6 +225,7 @@ while(running):
         player1.on_ground = False
     if keys[pygame.K_DOWN] and Yvel < speed:
         Yvel += 1
+        grav += 1
 
     Xvel *= friction
 
@@ -160,9 +233,16 @@ while(running):
         Yvel += gravity  # Apply gravity when not on the ground
     if player1.on_ground == True:
         Yvel = 1
-    print(player1.on_ground)
+    if grav < 15:
+        grav += gravity
+    if mob1.on_ground == True:
+        grav = 1
+        #print(player1.on_ground)
     player1.move(0, Yvel)
     player1.move(Xvel, 0)
+    mob1.move(0, grav)
+    mob1.mobPathFind()
     map.updateMap()
     pygame.draw.rect(screen, (255,0,0), pygame.Rect(player1.posx, player1.posy, tileSize, tileSize*2))
+    pygame.draw.rect(screen, (50,100,150), pygame.Rect(mob1.posx, mob1.posy, tileSize, tileSize*2))
     pygame.display.flip()
